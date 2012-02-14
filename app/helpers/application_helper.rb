85f1c3ab577eec5a01de39d75e096cdfd9fbc3ce
Tricia Morizio <pmorizio@stackbuilders.com>
2012-02-13 23:14:15 -0500

    Make a basic User model (including secure passwords)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    @logo = image_tag("logo.png", alt: "Sample App", class: "round")
+  end
+
   # Returns the full title on a per-page basis.			# Documentation comment
   def full_title(page_title)							# Method definition
     base_title = "Ruby on Rails Tutorial Sample App"	# Variable assignment
